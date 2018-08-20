37c1bca153f1c4fae9b1c72d2bf03cf47ac8267e
admin <admin@admins-macbook-air-5.local>
2018-08-20 16:06:35 +0700

    First Commit

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -10,8 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblHello: UILabel!
+    @IBOutlet weak var txtMessage: UITextField!
+    var count: Int = 0;
+    
+    @IBAction func clickButton(_ sender: Any) {
+        self.lblHello.text = "Hello \(self.txtMessage.text!) click num \(count)"
+    }
+    
+    @IBAction func changeTheme(_ sender: Any) {
+        for eachView in view.subviews {
+            if(self.count % 2 == 0){
+                view.backgroundColor = UIColor.black
+                eachView.backgroundColor = UIColor.black
+                if let uiLebel = eachView as? UILabel {
+                    uiLebel.textColor = UIColor.white
+                }
+            }else{
+                view.backgroundColor = UIColor.white
+                eachView.backgroundColor = UIColor.white
+                if let uiLebel = eachView as? UILabel {
+                    uiLebel.textColor = UIColor.black
+                }
+            }
+            
+        }
+        self.count += 1
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.lblHello.text = "Hello World!"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
